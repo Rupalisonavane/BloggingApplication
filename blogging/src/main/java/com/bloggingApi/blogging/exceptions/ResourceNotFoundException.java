d4c1d55defc1b6a2c42ee7d795adcc7f0833aeec
Rupali190593 <jtechrups1993@gmail.com>
2026-01-16 16:43:46 +0530

    changes commited

--- blogging/src/main/java/com/bloggingApi/blogging/exceptions/ResourceNotFoundException.java
+++ blogging/src/main/java/com/bloggingApi/blogging/exceptions/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ public class ResourceNotFoundException extends RuntimeException {
     String ResourceName;
     String fieldvalue;
     long fieldName;
-public ResourceNotFoundException(String resourceName, String fieldvalue, String fieldName) {
+public ResourceNotFoundException(String resourceName, String fieldvalue, long fieldName) {
 	super();
 	ResourceName = resourceName;
 	this.fieldvalue = fieldvalue;
--- blogging/src/main/java/com/bloggingApi/blogging/payloads/UserDto.java
+++ blogging/src/main/java/com/bloggingApi/blogging/payloads/UserDto.java
@@ -1,5 +1,41 @@
 package com.bloggingApi.blogging.payloads;
 
 public class UserDto {
+ private long id; 
+ private String email;
+  private String password;
+  private String about;
+  private String name;
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getAbout() {
+	return about;
+}
+public void setAbout(String about) {
+	this.about = about;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
 
+  
 }
